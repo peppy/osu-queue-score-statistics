5131e5791424df21e304e5a6fc788fe6343a4897
Dean Herbert <pe@ppy.sh>
2026-07-28 14:04:47 +0900

    Fix view deletion

--- osu.Server.Queues.ScoreStatisticsProcessor.Tests/DatabaseTest.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor.Tests/DatabaseTest.cs
@@ -89,7 +89,7 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
                 db.Execute("TRUNCATE TABLE `osu_user_performance_rank_highest`");
 
                 // Used in mark non-presrve tests.
-                db.Execute("TRUNCATE TABLE `multiplayer_playlist_item_scores`");
+                db.Execute("TRUNCATE TABLE `multiplayer_score_links`");
                 db.Execute("TRUNCATE TABLE `score_pins`");
             }
 
